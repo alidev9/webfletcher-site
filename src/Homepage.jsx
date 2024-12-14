7c6cef8bedafb05871072a1d1abc79a446846410
ali <159271119+alidev9@users.noreply.github.com>
2024-12-14 22:04:12 +0300

    Implemented the design of the navbar and added a cloud as a background svg

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,7 +1,26 @@
 
+import companyLogo from './assets/images/company-logo.png'
+import menuToggle from './assets/images/hamburger-menu-toggle.png'
+import heroImage1 from './assets/images/hero-image-1.png'
+import heroImage2 from './assets/images/hero-image-2.png'
+import targetGraphic from './assets/images/target-graphic.png'
+
 function Homepage(){
     return (
         <>
+            <nav>
+                <ul>
+                    <li className="homepage-link">
+                        <a href="#">
+                            <img src={companyLogo} alt="Webfletcher Logo" />
+                            <span>WebFletcher</span>
+                        </a>
+                    </li>
+                    <li>
+                        <img src={menuToggle} alt="Menu Toggle" />
+                    </li>
+                </ul>
+            </nav>
             <div className="hero-section-container">
                 <div className="hero-section-cta">
                     <h1>Your Vision, <span>Perfectly</span> Tailored</h1>
@@ -28,15 +47,15 @@ function Homepage(){
                         <div className="border-corner"></div>
                         <div className="border-corner"></div>
                         <div className="border-corner"></div>
-                        <img src="/images/hero-image-1.png" alt="" />
+                        <img src={heroImage1} alt="" />
                     </div>
                     <div className="hero-image-container">
                         <div className="border-corner"></div>
                         <div className="border-corner"></div>
                         <div className="border-corner"></div>
                         <div className="border-corner"></div>
-                        <img src="/images/hero-image-2.png" alt="" />
-                        </div>
+                        <img src={heroImage2} alt="" />
+                    </div>
                     <div className="hero-text-container">
                         <div className="border-corner"></div>
                         <div className="border-corner"></div>
@@ -44,7 +63,7 @@ function Homepage(){
                         <div className="border-corner"></div>
                         Be seen with <span> SEO</span>
                     </div>
-                    <img className="target-graphic" src="/images/target-graphic.png" alt="" />
+                    <img className="target-graphic" src={targetGraphic} alt="" />
                 </div>
             </div>
         </>
