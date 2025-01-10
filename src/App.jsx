db69bc686232faab3df0feec657ab2ae05df48d2
ali <159271119+alidev9@users.noreply.github.com>
2025-01-10 13:28:08 +0300

    The hamburger menu now smoothly goes in and out of view

--- src/App.jsx
+++ src/App.jsx
@@ -8,105 +8,89 @@ import { useState, useEffect } from 'react'
 import './styles/base.css'
 
 function App() {
-  const [hamburgerMenuOn, setHamburgerMenuOn] = useState(false);
-  function toggleHamburgerContainer(){
-    //This helps in setting specific properties for when html, body and #root are parents to the hamburger menu
-    document.querySelector('html').classList.toggle('hamburger-container-html');
-    document.querySelector('body').classList.toggle('hamburger-container-body');
-    document.querySelector('#root').classList.toggle('hamburger-container-root');
-  }
   function showHamburgerMenu(e){
-    toggleHamburgerContainer();
-    setHamburgerMenuOn(true);
+    document.querySelector('.hamburger-menu-container').classList.toggle('hamburger-menu-container-show');
   }
   function closeHamburgerMenu(e){
-    toggleHamburgerContainer();
-    setHamburgerMenuOn(false);
+    document.querySelector('.hamburger-menu-container').classList.toggle('hamburger-menu-container-show');
   }
   /*TODO: Make the name of the current page bold in the hamburger menu*/
   useEffect(() => {
     const urlPath = window.location.pathname;
   }, [])
-  if(!hamburgerMenuOn){
-    return (
-        <>
-          <header>
-              <nav>
-                    <div className="navbar-inner-wrapper">
-                        <div className="homepage-link">
-                            <Link to='/'>
-                              <img src={companyLogo} alt="Webfletcher Logo" />
-                              <span>WebFletcher</span>
-                            </Link>
-                        </div>
-                        <ul className="navbar-links-container">
-                          <li>About</li>
-                          <li>Services</li>
-                          <li>Portfolio</li>
-                          <li>Support</li>
-                        </ul>
-                        <div className="account-button-container">
-                          <button>Sign In</button>
-                          <button>Register</button>
-                        </div>
-                        <img src={menuToggle} alt="Menu Toggle" onClick={showHamburgerMenu}/>
-                    </div>
-              </nav>
-          </header>
-        <Routes>
-          <Route path='/' element={<Homepage />}/>
-        </Routes>
-        <footer>
-          <div className="company-logo-description-wrapper">
-            <div className="company-logo">
-              <img src={companyLogo} alt="" />
-              <span>WebFletcher</span>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat metus eget dolor faucibus, vel maximus nulla scelerisque</p>
-          </div>
-          <div className="links-wrapper">
-            <div className="resources-links-wrapper footer-wrapper">
-              <h2>Resources</h2>
-              <div>FAQ</div>
-              <div>Support</div>
-              <div>Careers</div>
-            </div>
-            <div className="legal-links-wrapper footer-wrapper">
-              <h2>Legal</h2>
-              <div>Privacy Policy</div>
-              <div>Terms of Service</div>
-            </div>
-            <div className="contact-info-wrapper footer-wrapper">
-              <h2>Contact Us</h2>
-              <div className="email">example@hotmail.com</div>
-              <div className="phone-number">+123456789</div>
-            </div>
-          </div>
-          <div className="copyright-text">Copyright © 2024 WebFletcher. All rights reserved</div>
-        </footer>
-      </>
-    );
-  } else {
     return (
       <>
-        {/*using a div as this is not an actual header*/}
-        <div className="hamburger-header">
-          <Link to='/'>
-            <img src={companyLogo} alt="Webfletcher Logo" />
-            <span>WebFletcher</span>
-            <img src={closeMenuToggle} alt="Close Hamburger Menu" onClick={closeHamburgerMenu} />
-          </Link>
-        </div>
-        <ul className="hamburger-links-container">
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Support</li>
-        </ul>
-        <button className="order-button">Order a Site</button>
+          <div className="page-wrapper">
+            <header>
+                <nav>
+                      <div className="navbar-inner-wrapper">
+                          <div className="homepage-link">
+                              <Link to='/'>
+                                <img src={companyLogo} alt="Webfletcher Logo" />
+                                <span>WebFletcher</span>
+                              </Link>
+                          </div>
+                          <ul className="navbar-links-container">
+                            <li>About</li>
+                            <li>Services</li>
+                            <li>Portfolio</li>
+                            <li>Support</li>
+                          </ul>
+                          <div className="account-button-container">
+                            <button>Sign In</button>
+                            <button>Register</button>
+                          </div>
+                          <img src={menuToggle} alt="Menu Toggle" onClick={showHamburgerMenu}/>
+                      </div>
+                </nav>
+            </header>
+                    <Routes>
+            <Route path='/' element={<Homepage />}/>
+                    </Routes>
+                    <footer>
+            <div className="company-logo-description-wrapper">
+              <div className="company-logo">
+                <img src={companyLogo} alt="" />
+                <span>WebFletcher</span>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat metus eget dolor faucibus, vel maximus nulla scelerisque</p>
+            </div>
+            <div className="links-wrapper">
+              <div className="resources-links-wrapper footer-wrapper">
+                <h2>Resources</h2>
+                <div>FAQ</div>
+                <div>Support</div>
+                <div>Careers</div>
+              </div>
+              <div className="legal-links-wrapper footer-wrapper">
+                <h2>Legal</h2>
+                <div>Privacy Policy</div>
+                <div>Terms of Service</div>
+              </div>
+              <div className="contact-info-wrapper footer-wrapper">
+                <h2>Contact Us</h2>
+                <div className="email">example@hotmail.com</div>
+                <div className="phone-number">+123456789</div>
+              </div>
+            </div>
+            <div className="copyright-text">Copyright © 2024 WebFletcher. All rights reserved</div>
+                    </footer>
+          </div>
+          {/*using a div as this is not an actual header*/}
+          <div className="hamburger-menu-container">
+            <div className="hamburger-header">
+              <img src={closeMenuToggle} alt="Close Hamburger Menu" onClick={closeHamburgerMenu} />
+            </div>
+            <ul className="hamburger-links-container">
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>Support</li>
+            </ul>
+            <button className="order-button">Order a Site</button>
+          </div>
       </>
-    )
-  }
+    );
 }
 export default App;
